c1443bd39b3cf97e5c1ffa883ae16d86512f2fb2
Silas dos Santos Caimi <silascaimi@live.com>
2021-03-17 17:46:07 -0300

    first commit

---
+++ src/test/java/com/silascaimi/demoacmeap/DemoAcmeApApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silascaimi.demoacmeap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAcmeApApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
